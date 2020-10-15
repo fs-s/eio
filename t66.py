6d9a54d1e9e336f91fa12440a53972163d77a625
fs-s <55434294+fs-s@users.noreply.github.com>
2020-10-15 14:06:27 +0300

    Update t66.py

--- t66.py
+++ t66.py
@@ -1,25 +1,16 @@
 f = open("nimsis0.txt","r")
 n = f.readline()
-
 y = {}
 y2 = {}
-y3={}
-
 for x in range(int(n)):
     name = f.readline()
     splitted = name.split()
     y[splitted[0]] = splitted[1]
-
-
-
 for r in range(int(n)):
     name = f.readline()
     splitted2 = name.split()
     y2[splitted2[0]] = splitted2[1]
-
-
 for z in y:
     for v in y2:
         if z == v:
-            y3[y[z]] = y2[v]
-print(y3)
+            print(y[z] + " " + y2[v])
